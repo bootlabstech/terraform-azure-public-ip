b11e9d301ae13aac20521ee445555cda588f1084
lalita2407 <lalitalallu02@gmail.com>
2023-09-24 10:25:26 +0530

    changed provider to 3.64 and code cleanup

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 resource "azurerm_public_ip" "public_ip" {
-  name                = var.public_ip_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  allocation_method   = var.allocation_method
-  sku                 = var.sku
+  name                 = var.name
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  allocation_method    = var.allocation_method
+  sku                  = var.sku
+  ddos_protection_mode = var.ddos_protection_mode
+  sku_tier             = var.sku_tier
   lifecycle {
     ignore_changes = [
       tags,
--- variables.tf
+++ variables.tf
@@ -1,23 +1,31 @@
+# Public IP
+variable "name" {
+  type        = string
+  description = "Specifies the name of the Public IP. Changing this forces a new Public IP to be created."
+}
+variable "resource_group_name" {
+  description = "The name of the Resource Group where this Public IP should exist."
+  type        = string
+}
 variable "location" {
   type        = string
-  description = "Specifies the supported Azure location where the Public IP should exist. Changing this forces a new resource to be created."
+  description = "Specifies the supported Azure location where the Public IP should exist."
 }
-
-variable "public_ip_name" {
-  type        = string
-  description = "The name of the Resource Group where this FrontDoor Profile should exist"
-}
-
 variable "allocation_method" {
   type        = string
   description = "Defines the allocation method for this IP address. Possible values are Static or Dynamic."
 }
-
-variable "resource_group_name" {
-  description = " The name of the Resource Group where this Public IP should exist."
+variable "sku" {
+  description = "The SKU of the Public IP. Accepted values are Basic and Standard. Defaults to Basic. Changing this forces a new resource to be created."
   type        = string
 }
-variable "sku" {
-  description = " The name of the Resource Group where this Public IP should exist."
+variable "ddos_protection_mode" {
+  description = "The DDoS protection mode of the public IP. Possible values are Disabled, Enabled, and VirtualNetworkInherited. Defaults to VirtualNetworkInherited.."
   type        = string
+  default     = "VirtualNetworkInherited"
+}
+variable "sku_tier" {
+  description = "The SKU Tier that should be used for the Public IP. Possible values are Regional and Global. Defaults to Regional. Changing this forces a new resource to be created."
+  type        = string
+  default     = "Regional"
 }
